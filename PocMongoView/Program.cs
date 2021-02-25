0139850b6e783d4ab00a3389884dff7b9c22242f
André Mendes <andre.marcondes@betalearning.com.br>
2021-02-25 09:23:18 -0300

    criacao de view com mongo

--- PocMongoView/Program.cs
+++ PocMongoView/Program.cs
@@ -1,12 +1,41 @@
-﻿using System;
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System.Threading.Tasks;
 
-namespace PocMongoView
+namespace MongoDBQuery
 {
     class Program
     {
+        private const string CONNECTION_STRING = "";
+        private const string DATABASE_NAME = "";
+        private const string COLLECTION_NAME = "";
+        private const string FIELD_TO_REMOVE = "";
+        private const string VIEW_NAME = "";
+
+        static async Task _Main()
+        {
+            IMongoClient client = new MongoClient(CONNECTION_STRING);
+            IMongoDatabase database = client.GetDatabase(DATABASE_NAME);
+            IMongoCollection<BsonDocument> collection = database.GetCollection<BsonDocument>(COLLECTION_NAME);
+
+
+            PipelineDefinition<BsonDocument, BsonDocument> pipeline = new BsonDocument[]
+            {
+                //Operadores que podem ser usados na pipeline
+                //https://docs.mongodb.com/manual/reference/operator/aggregation/index.html
+                new BsonDocument("$project", new BsonDocument()
+                        .Add(FIELD_TO_REMOVE, 0.0))
+            };
+
+
+            database.CreateView<BsonDocument, BsonDocument>
+                (VIEW_NAME, COLLECTION_NAME, pipeline, null);
+         
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            _Main().Wait();
         }
     }
 }
---
+++ PocMongoView/TemplateResponseDTO.cs
@@ -0,0 +1,54 @@
+﻿using MongoDB.Bson.Serialization.Attributes;
+using System;
+using System.Collections.Generic;
+
+namespace MongoDBQuery
+{
+    public class Status
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Category
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string description { get; set; }
+    }
+
+    public class Language
+    {
+        public int id { get; set; }
+        public string code { get; set; }
+        public string name { get; set; }
+    }
+
+    public class Button
+    {
+        public string type { get; set; }
+        public string text { get; set; }
+    }
+
+    public class Template
+    {
+        public string type { get; set; }
+        public string text { get; set; }
+        public List<Button> buttons { get; set; }
+    }
+
+    public class TemplateSettings
+    {
+        [BsonId]
+        public string id { get; set; }
+        public string wa_id { get; set; }
+        public Status status { get; set; }
+        public Category category { get; set; }
+        public Language language { get; set; }
+        public string name { get; set; }
+        public List<Template> components { get; set; }
+        public DateTime created_at { get; set; }
+        public DateTime updated_at { get; set; }
+    }
+}
